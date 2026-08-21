f933f3ad0a6971a2bae50e837a6824913fa78778
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-08-21 12:45:58 +0100

    chore: remove obsolete deprecated debug scripts (#198)

--- backend/deprecated/check_db.py
+++
@@ -1,9 +0,0 @@
-
-from backend.database import SessionLocal
-from backend.models import CabinetConfig
-
-db = SessionLocal()
-configs = db.query(CabinetConfig).all()
-for c in configs:
-    print(f"Owner: {c.owner_id}, Primary Color: {c.primary_color}, Margin Top: {c.margin_top}, Margin Bottom: {c.margin_bottom}")
-db.close()
