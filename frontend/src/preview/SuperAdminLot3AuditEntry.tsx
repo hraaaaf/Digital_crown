b0f13a105f3835e58564d98e73c12920ab03696e
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-19 14:44:30 +0100

    test(v1-07): render toasts in LOT3 visual harness

--- frontend/src/preview/SuperAdminLot3AuditEntry.tsx
+++ frontend/src/preview/SuperAdminLot3AuditEntry.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import '../index.css';
 import { SuperAdminDashboard } from '../features/superadmin/SuperAdminDashboard';
 
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <SuperAdminDashboard />
+      <Toaster position="top-right" />
     </BrowserRouter>
   </React.StrictMode>,
 );
