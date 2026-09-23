ee486b723b546e265411f8e6022047c35c5f9a0a
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-23 21:33:54 +0100

    tmp: merge CUST-02 and PC-08 Alembic heads

---
+++ alembic/versions/v107merge0016_merge_cust02_pc08_heads.py
@@ -0,0 +1,18 @@
+"""Merge CUST-02 and PC-08 migration heads.
+
+Revision ID: v107merge0016
+Revises: cust02000015, pc080000015
+"""
+
+revision = "v107merge0016"
+down_revision = ("cust02000015", "pc080000015")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
